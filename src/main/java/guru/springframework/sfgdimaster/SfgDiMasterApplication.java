ba7fec974d1acbf368a7c55accaf0db98666e996
m02242a <Brian.Harford@experian.com>
2021-11-02 09:18:03 -0400

    added spring components using spring initializr

---
+++ src/main/java/guru/springframework/sfgdimaster/SfgDiMasterApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdimaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgdimaster/SfgDiMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdimaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
